d5d1cd96c1a5015cdae9c86cbfb8bc5dc6d24afc
jesus  henriquez mura <jesus.henriquez.m@usach.cl>
2021-11-29 14:57:39 -0300

    add passenger to project

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,12 +1,22 @@
 export enum RabbitMQ {
   UserQueue = "users",
+  PassengerQueue = "passengers",
 }
 
-export enum UserMSG{
-    CREATE='CREATED_USER',
-    FIND_ALL='FIND_ALL_USERS',
-    FIND_ONE='FIND_ONE_USER',
-    UPDATE='UPDATE_USER',
-    DELETED='DELETED_USER',
-    VALID_USER='VALID_USER',
+export enum UserMSG {
+  CREATE = "CREATED_USER",
+  FIND_ALL = "FIND_ALL_USERS",
+  FIND_ONE = "FIND_ONE_USER",
+  UPDATE = "UPDATE_USER",
+  DELETED = "DELETED_USER",
+  VALID_USER = "VALID_USER",
+}
+
+export enum PassengerMSG {
+  CREATE = "CREATED_PASSENGER",
+  FIND_ALL = "FIND_ALL_PASSENGERS",
+  FIND_ONE = "FIND_ONE_PASSENGER",
+  UPDATE = "UPDATE_PASSENGER",
+  DELETED = "DELETED_PASSENGER",
+  VALID_USER = "VALID_PASSENGER",
 }
--- src/common/proxy/client-proxy.ts
+++ src/common/proxy/client-proxy.ts
@@ -16,4 +16,14 @@ export class ClientProxySuperFlights {
             }
         })
     }
+
+    clientProxyPassengers(): ClientProxy{
+        return ClientProxyFactory.create({
+            transport: Transport.RMQ,
+            options:{
+                urls: this.config.get('AMQP_URL'),
+                queue: RabbitMQ.PassengerQueue,
+            }
+        })
+    }
 }
