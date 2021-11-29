ba7112c1bebc203cc0783f4df90e8f04383adccf
jesus  henriquez mura <jesus.henriquez.m@usach.cl>
2021-11-29 13:40:25 -0300

    configs rabbitMQ to project

---
+++ src/common/proxy/client-proxy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ClientProxy, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { RabbitMQ } from "../constants";
+
+@Injectable()
+export class ClientProxySuperFlights {
+    constructor(private readonly config:ConfigService){}
+
+    clientProxyUsers(): ClientProxy{
+        return ClientProxyFactory.create({
+            transport: Transport.RMQ,
+            options:{
+                urls: this.config.get('AMQP_URL'),
+                queue: RabbitMQ.UserQueue,
+            }
+        })
+    }
+}
